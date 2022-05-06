04da93420f6d45ed3637b494e444e67e9e1836db
Régis Meyssonnier <67794100+regismeyssonnier@users.noreply.github.com>
2022-05-06 15:28:58 +0200

    Add files via upload

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-function{
+function hello(){
     alert("hello world");
 }
 
@@ -6,3 +6,14 @@ function{
  * ************Menu Function
  * @param {any} tt
  */
+function blink() {
+    setInterval(() => {
+        if (Date.now() % 10 == 0) {
+            document.getElementById("blinkp").style.color = "red";
+        }
+        else {
+            document.getElementById("blinkp").style.color = "white";
+        }
+    }, 1000);
+}
+
