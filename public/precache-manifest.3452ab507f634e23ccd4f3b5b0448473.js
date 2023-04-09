ce0276925bff355d627226bf1853a72ac4e7a21e
Régis Meyssonnier <67794100+regismeyssonnier@users.noreply.github.com>
2023-04-09 11:47:48 +0200

    Add files via upload

---
+++ public/precache-manifest.3452ab507f634e23ccd4f3b5b0448473.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8edd5cd6aea58944b5fe",
+    "url": "/static/css/main.fefe77f4.chunk.css"
+  },
+  {
+    "revision": "8edd5cd6aea58944b5fe",
+    "url": "/static/js/main.8edd5cd6.chunk.js"
+  },
+  {
+    "revision": "54087e177e7c5a193df0",
+    "url": "/static/js/1.54087e17.chunk.js"
+  },
+  {
+    "revision": "e04c2c57f3e67d3b76e5",
+    "url": "/static/js/2.e04c2c57.chunk.js"
+  },
+  {
+    "revision": "b3d155d33fc8f3126447",
+    "url": "/static/js/runtime~main.b3d155d3.js"
+  },
+  {
+    "revision": "a260ca407670cd37e90c84dd6cfe57a0",
+    "url": "/index.html"
+  }
+];
